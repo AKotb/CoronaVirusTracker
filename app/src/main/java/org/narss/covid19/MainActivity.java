4be61fc5bce0e118f8fb2c2c8495e1ecfd96906a
Ahmed Kotb <a7madkotb@gmail.com>
2020-04-21 14:30:01 +0200

    The following changes have been applied 1- A new custom_toast xml file has been added 2- Assign an id to the ConstraintLayout in activity_main.xml 3- MainActivity.java has been modified to carry out OnPolygonClickListner function to display info-window when a polygon is clicked.

--- app/src/main/java/org/narss/covid19/MainActivity.java
+++ app/src/main/java/org/narss/covid19/MainActivity.java
@@ -1,7 +1,11 @@
 package org.narss.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Dialog;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
 
 import org.narss.covid19.dbhelper.DBHelper;
@@ -16,21 +20,43 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
+
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnPolygonClickListener {
 
     protected GoogleApiClient mGoogleApiClient;
     DBHelper db;
     public List<Hospital> hospitalList;
+    int x = 0;
+    int y = 0;
+    private PopupWindow mPopupWindow;
+    private Button closeBtn;
+    private ConstraintLayout mConstraintLayout;
+    Projection projection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +67,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
         buildGoogleApiClient();
         mGoogleApiClient.connect();
+        closeBtn = (Button) findViewById(R.id.btn_close);
 
         db = new DBHelper(this);
 
@@ -72,6 +99,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         UiSettings mapSettings;
+        projection = googleMap.getProjection();
         mapSettings = googleMap.getUiSettings();
         for(int i=0; i<hospitalList.size(); i++)
         {
@@ -86,7 +114,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                         new LatLng(30.139, 31.282), new LatLng(30.141, 31.278), new LatLng(30.1421, 31.275), new LatLng(30.141, 31.273)
                         ,new LatLng(30.143, 31.264))
                 .strokeColor(Color.RED)
-                .fillColor(Color.BLUE));
+                .fillColor(Color.LTGRAY)
+                .clickable(true)).setTag("Area Name: Bahtim - Al Qalyubia Governorate\n" +
+                                        "Quarantine Start Date: 8-4-2020\n" +
+                                        "Quarantine End Date: 5-5-2020\n");
 
         //El-Hayatim
         googleMap.addPolygon(new PolygonOptions()
@@ -94,7 +125,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                         new LatLng(30.903525, 31.100985), new LatLng(30.901095, 31.101243), new LatLng(30.900948, 31.103431),new LatLng(30.903268, 31.105577), new LatLng(30.909675, 31.105147),
                         new LatLng(30.910485, 31.111327), new LatLng(30.917407, 31.112357))
                 .strokeColor(Color.RED)
-                .fillColor(Color.BLUE));
+                .fillColor(Color.LTGRAY)
+                .clickable(true)).setTag("Area Name: El-Hayatim - Al Gharbia Governorate\n" +
+                                        "Quarantine Start Date: 6-4-2020\n" +
+                                        "Quarantine End Date: 3-5-2020\n");
 
         //Moatamedia
         googleMap.addPolygon(new PolygonOptions()
@@ -103,7 +137,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                         ,new LatLng(30.050612, 31.177873),new LatLng(30.051911, 31.183189),new LatLng(30.046028, 31.188421),new LatLng(30.044007, 31.192278)
                         ,new LatLng(30.044458, 31.193446),new LatLng(30.059543, 31.190027))
                 .strokeColor(Color.RED)
-                .fillColor(Color.BLUE));
+                .fillColor(Color.LTGRAY)
+                .clickable(true)).setTag("Area Name: Moatamedia - Al Giza Governorate\n" +
+                                        "Quarantine Start Date: 13-4-2020\n" +
+                                        "Quarantine End Date: 10-5-2020\n");
 
         //Shobra Elbahow
         googleMap.addPolygon(new PolygonOptions()
@@ -111,12 +148,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                         new LatLng(30.949028,31.335306), new LatLng(30.948209, 31.344932), new LatLng(30.954009, 31.355487),
                         new LatLng(30.96231, 31.353471), new LatLng(30.963856,31.348804))
                 .strokeColor(Color.RED)
-                .fillColor(Color.BLUE));
+                .fillColor(Color.LTGRAY)
+                .clickable(true)).setTag("Area Name: Shobra El-bahow - Al Dakahlia Governorate\n" +
+                                        "Quarantine Start Date: 16-4-2020\n" +
+                                        "Quarantine End Date: 13-5-2020\n");
+
+        googleMap.setOnPolygonClickListener(this);
 
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(26.8206, 30.8025), 6));
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         googleMap.setTrafficEnabled(true);
         mapSettings.setZoomControlsEnabled(true);
+        mapSettings.setMyLocationButtonEnabled(true);
     }
     //----------------------------------------------------------------------------------------------
     @Override
@@ -180,6 +223,37 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         toSpinner.setSelection(0);
         results = "";
         details = "";*/
+    }
+
+    @Override
+    public void onPolygonClick(Polygon polygon) {
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast,
+                (ViewGroup) findViewById(R.id.custom_toast_container));
+        TextView text = (TextView) layout.findViewById(R.id.text);
+        text.setText(polygon.getTag().toString());
+        mConstraintLayout = (ConstraintLayout) findViewById(R.id.cl);
+
+        //PopupWindow(View contentView, int width, int height)
+        mPopupWindow = new PopupWindow(
+                layout, ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        mPopupWindow.setTouchable(true);
+        if(Build.VERSION.SDK_INT>=21){
+            mPopupWindow.setElevation(5.0f);
+        }
+        closeBtn = (Button) layout.findViewById(R.id.btn_close);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Dismiss the popup window
+                mPopupWindow.dismiss();
+            }
+        });
+
+        mPopupWindow.showAtLocation(mConstraintLayout, Gravity.CENTER,x,y);
     }
     //----------------------------------------------------------------------------------------------
 }
